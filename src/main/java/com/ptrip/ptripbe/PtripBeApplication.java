1566983e4f29364b480296cd4744cfed6ddf7d8a
soyoung <ssssy93@icloud.com>
2026-05-10 08:34:24 +0900

    init: 백엔드 프로젝트 초기 생성

---
+++ src/main/java/com/ptrip/ptripbe/PtripBeApplication.java
@@ -0,0 +1,13 @@
+package com.ptrip.ptripbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtripBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PtripBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ptrip/ptripbe/PtripBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ptrip.ptripbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtripBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
